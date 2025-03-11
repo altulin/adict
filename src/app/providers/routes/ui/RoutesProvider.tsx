3817268a1dc4fb3b0e50e1f4b22a5a51bacedb29
altln <al.tulin@gmail.com>
2025-03-11 08:52:37 +0800

    path

--- src/app/providers/routes/ui/RoutesProvider.tsx
+++ src/app/providers/routes/ui/RoutesProvider.tsx
@@ -8,31 +8,34 @@ import { FC } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router";
 
 const RoutesProviderFC: FC = () => {
-  const router = createBrowserRouter([
-    {
-      path: EnumRoutes.MAIN,
-      element: <BaseLayout />,
-      children: [
-        {
-          path: "/",
-          element: <PageHome />,
-        },
-        {
-          path: `/${EnumRoutes.SPEAKERS}`,
-          element: <Speakers />,
-        },
-        {
-          path: `/${EnumRoutes.REGISTRATION}`,
-          element: <Registration />,
-        },
-      ],
-    },
+  const router = createBrowserRouter(
+    [
+      {
+        path: EnumRoutes.MAIN,
+        element: <BaseLayout />,
+        children: [
+          {
+            path: "/",
+            element: <PageHome />,
+          },
+          {
+            path: `/${EnumRoutes.SPEAKERS}`,
+            element: <Speakers />,
+          },
+          {
+            path: `/${EnumRoutes.REGISTRATION}`,
+            element: <Registration />,
+          },
+        ],
+      },
 
-    {
-      path: "*",
-      element: <div>404</div>,
-    },
-  ]);
+      {
+        path: "*",
+        element: <div>404</div>,
+      },
+    ],
+    // { basename: import.meta.env.BASE_URL },
+  );
 
   return <RouterProvider router={router} />;
 };
